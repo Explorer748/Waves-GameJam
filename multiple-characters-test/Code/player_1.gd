a6ccb11e1d849e44d5e404522eeac695ce010c71
Explorer748 <jonathan-frederic.krause@t-online.de>
2025-11-25 15:21:45 +0100

    Working movement script with jumping, sliding, dashing and walking
    There are sometimes slight w,a,s,d input delays on my laptop so we should optimize it

--- multiple-characters-test/Code/player_1.gd
+++ multiple-characters-test/Code/player_1.gd
@@ -1,29 +1,47 @@
 extends CharacterBody2D
 
 
-var speed = 1000.0
-var dash_speed = 100
-const JUMP_VELOCITY = -400.0
+var speed = 0.0
+var dash_speed = 20000.0
+var dash = false
+const JUMP_VELOCITY = -600.0
 var activ = true
 var stamina = 100
 
 func _physics_process(delta: float) -> void:
 	$UI/StaminaBar.value = stamina
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and activ:
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_just_pressed("dash") and activ and stamina >= 50:
+		$Timer.start()
+		dash = true
+		stamina -= 50
+		
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction and activ:	
-		velocity.x = direction * (speed + dash_speed)
+		if dash == true:
+			
+			velocity.x = direction * dash_speed
+		else:	
+			velocity.x = direction * speed 
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-	if not is_on_floor():
+		
+		
+	if dash == true:
+			$AnimationPlayer.play("dash")
+	elif not is_on_floor() :
 		$AnimationPlayer.play("jump")
 	else:
-		if velocity.x != 0:
+		if Input.is_action_pressed("ui_down"):
+			$AnimationPlayer.play("slide")
+		elif velocity.x != 0:
 			$AnimationPlayer.play("run")
 		else:
 			$AnimationPlayer.play("RESET")
@@ -32,15 +50,7 @@ func _physics_process(delta: float) -> void:
 		transform.x = Vector2(-1.0,0.0)
 	if direction == 1:
 		transform.x = Vector2(1.0,0.0)
-		
-	if Input.is_action_just_pressed("dash") and activ and stamina >= 25:
-		$AnimationPlayer.play("dash")
-		dash_speed = 10000
-		stamina -= 25
-	else:
-		dash_speed = 0
-		
-		
+			
 	if activ:
 		$CollisionShape2D/Camera2D.enabled = true
 		$UI/StaminaBar.visible = true
@@ -52,3 +62,8 @@ func _physics_process(delta: float) -> void:
 		speed = 200
 		
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	dash = false
+	pass # Replace with function body.
