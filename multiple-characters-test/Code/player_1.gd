5addf0149970eb4db0a5776ca7d32997b5646247
Explorer748 <jonathan-frederic.krause@t-online.de>
2025-11-25 13:05:54 +0100

    Working jump and basic run animation
    Working jump and basic run animation. Dash animation needs to be reworked and slide animation is currently deactivate because it causes to get the player stuck in the ground.

--- multiple-characters-test/Code/player_1.gd
+++ multiple-characters-test/Code/player_1.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-var speed = 300.0
+var speed = 1000.0
 var dash_speed = 100
 const JUMP_VELOCITY = -400.0
 var activ = true
 var stamina = 100
 
 func _physics_process(delta: float) -> void:
-
+	$UI/StaminaBar.value = stamina
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -16,23 +16,39 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction and activ:
+	if direction and activ:	
 		velocity.x = direction * (speed + dash_speed)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+	if not is_on_floor():
+		$AnimationPlayer.play("jump")
+	else:
+		if velocity.x != 0:
+			$AnimationPlayer.play("run")
+		else:
+			$AnimationPlayer.play("RESET")
+
+	if direction == -1:
+		transform.x = Vector2(-1.0,0.0)
+	if direction == 1:
+		transform.x = Vector2(1.0,0.0)
 		
 	if Input.is_action_just_pressed("dash") and activ and stamina >= 25:
+		$AnimationPlayer.play("dash")
 		dash_speed = 10000
 		stamina -= 25
 	else:
 		dash_speed = 0
 		
+		
 	if activ:
 		$CollisionShape2D/Camera2D.enabled = true
-		speed = 300
+		$UI/StaminaBar.visible = true
+		speed = 1000
 		
 	else:
 		$CollisionShape2D/Camera2D.enabled = false
+		$UI/StaminaBar.visible = false
 		speed = 200
 		
 	move_and_slide()
